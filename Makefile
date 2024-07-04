b483333887bf4591bcbca6b72a9f1db0fbf6fa57
Mauricio Castro <macastro@redhat.com>
2024-07-04 08:20:18 -0300

    init. ported from hcreport operator controller.

---
+++ Makefile
@@ -0,0 +1,111 @@
+# Image URL to use all building/pushing image targets
+IMG ?= kcdump:latest
+CONTAINER_FILE ?= Containerfile
+# ENVTEST_K8S_VERSION refers to the version of kubebuilder assets to be downloaded by envtest binary.
+ENVTEST_K8S_VERSION = 1.27.1
+
+# Get the currently used golang install path (in GOPATH/bin, unless GOBIN is set)
+ifeq (,$(shell go env GOBIN))
+GOBIN=$(shell go env GOPATH)/bin
+else
+GOBIN=$(shell go env GOBIN)
+endif
+
+# CONTAINER_TOOL defines the container tool to be used for building images.
+# Be aware that the target commands are only tested with Docker which is
+# scaffolded by default. However, you might want to replace it to use other
+# tools. (i.e. podman)
+CONTAINER_TOOL ?= podman
+
+# Setting SHELL to bash allows bash commands to be executed by recipes.
+# Options are set to exit when a recipe line exits non-zero or a piped command fails.
+SHELL = /usr/bin/env bash -o pipefail
+.SHELLFLAGS = -ec
+
+.PHONY: all
+all: build
+
+##@ General
+
+# The help target prints out all targets with their descriptions organized
+# beneath their categories. The categories are represented by '##@' and the
+# target descriptions by '##'. The awk commands is responsible for reading the
+# entire set of makefiles included in this invocation, looking for lines of the
+# file as xyz: ## something, and then pretty-format the target and help. Then,
+# if there's a line with ##@ something, that gets pretty-printed as a category.
+# More info on the usage of ANSI control characters for terminal formatting:
+# https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_parameters
+# More info on the awk command:
+# http://linuxcommand.org/lc3_adv_awk.php
+
+.PHONY: help
+help: ## Display this help.
+	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make \033[36m<target>\033[0m\n"} /^[a-zA-Z_0-9-]+:.*?##/ { printf "  \033[36m%-15s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
+
+##@ Development
+
+.PHONY: fmt
+fmt: ## Run go fmt against code.
+	go fmt ./...
+
+.PHONY: vet
+vet: ## Run go vet against code.
+	go vet ./...
+
+.PHONY: test
+test: manifests generate fmt vet envtest ## Run tests.
+	KUBEBUILDER_ASSETS="$(shell $(ENVTEST) use $(ENVTEST_K8S_VERSION) --bin-dir $(LOCALBIN) -p path)" go test ./... -coverprofile cover.out
+
+##@ Build
+
+#.PHONY: vendor
+vendor:
+#	go mod tidy
+#	go mod vendor
+
+.PHONY: build
+build: fmt vet ## Build manager binary.
+	go build -o bin/kcdump cmd/main.go
+
+# If you wish built the manager image targeting other platforms you can use the --platform flag.
+# (i.e. docker build --platform linux/arm64 ). However, you must enable docker buildKit for it.
+# More info: https://docs.docker.com/develop/develop-images/build_enhancements/
+.PHONY: container-build
+container-build: test ## Build docker image with the manager.
+	$(CONTAINER_TOOL) build -f ${CONTAINER_FILE} -t ${IMG} .
+
+.PHONY: container-push
+container-push: ## Push docker image with the manager.
+	$(CONTAINER_TOOL) push ${IMG}
+
+# PLATFORMS defines the target platforms for  the manager image be build to provide support to multiple
+# architectures. (i.e. make docker-buildx IMG=myregistry/mypoperator:0.0.1). To use this option you need to:
+# - able to use docker buildx . More info: https://docs.docker.com/build/buildx/
+# - have enable BuildKit, More info: https://docs.docker.com/develop/develop-images/build_enhancements/
+# - be able to push the image for your registry (i.e. if you do not inform a valid value via IMG=<myregistry/image:<tag>> then the export will fail)
+# To properly provided solutions that supports more than one platform you should use this option.
+PLATFORMS ?= linux/arm64,linux/amd64,linux/s390x,linux/ppc64le
+.PHONY: container-buildx
+container-buildx: test ## Build and push docker image for the manager for cross-platform support
+	# copy existing Dockerfile and insert --pla tform=${BUILDPLATFORM} into Dockerfile.cross, and preserve the original Dockerfile
+	sed -e '1 s/\(^FROM\)/FROM --platform=\$$\{BUILDPLATFORM\}/; t' -e ' 1,// s//FROM --platform=\$$\{BUILDPLATFORM\}/' Dockerfile > Dockerfile.cross
+	- $(CONTAINER_TOOL) buildx create --name project-v3-builder
+	$(CONTAINER_TOOL) buildx use project-v3-builder
+	- $(CONTAINER_TOOL) buildx build --push --platform=$(PLATFORMS) --tag ${IMG} -f Dockerfile.cross .
+	- $(CONTAINER_TOOL) buildx rm project-v3-builder
+	rm Dockerfile.cross
+
+##@ Deployment
+
+ifndef ignore-not-found
+  ignore-not-found = false
+endif
+
+##@ Build Dependencies
+
+## Location to install dependencies to
+LOCALBIN ?= $(shell pwd)/bin
+$(LOCALBIN):
+	mkdir -p $(LOCALBIN)
+
+## Tool Binaries
---
+++ cmd/main.go
@@ -0,0 +1,166 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"go.uber.org/zap"
+
+	"github.com/mauricioscastro/hcreport/pkg/kc"
+	kcli "github.com/mauricioscastro/hcreport/pkg/kc"
+	"github.com/mauricioscastro/hcreport/pkg/util/log"
+	"github.com/mauricioscastro/hcreport/pkg/yjq"
+)
+
+type nsExcludeList []string
+type gvkExcludeList []string
+
+var (
+	logger = log.Logger().Named("hcr")
+
+	// cli dump options
+	home        string
+	gzip        bool
+	tgz         bool
+	prune       bool
+	nologs      bool
+	ns          bool
+	dontsplitns bool
+	dontsplitgv bool
+	gvk         bool
+	xns         nsExcludeList
+	xgvk        gvkExcludeList
+	targetDir   string
+	format      string
+	kubeconfig  string
+	context     string
+	logLevel    string
+)
+
+func init() {
+	yjq.SilenceYqLogs()
+	var err error
+	home, err = os.UserHomeDir()
+	if err != nil {
+		logger.Error("reading home info", zap.Error(err))
+		os.Exit(-1)
+	}
+	if home == "/" {
+		home = ""
+	}
+}
+
+func main() {
+	flag.BoolVar(&nologs, "nologs", false, "do not output pod's logs")
+	flag.BoolVar(&gzip, "gzip", false, "gzip output")
+	flag.BoolVar(&tgz, "tgz", false, "a gzipped tar file is created at targetDir level with its contents. will turn off gzip option.")
+	flag.BoolVar(&prune, "prune", false, "prunes targetDir after archiving. implies tgz option. if tgz option is not used it does nothing.")
+	flag.BoolVar(&ns, "ns", false, "print namespaces list")
+	flag.BoolVar(&gvk, "gvk", false, "print group version kind with format 'gv,k'")
+	flag.BoolVar(&dontsplitns, "dontSplitns", false, "do not split namespaced items into directories with their namespace name")
+	flag.BoolVar(&dontsplitgv, "dontSplitgv", false, "do not split groupVersion in separate files. implies -dontSplitns and -format 'yaml' or 'json_lines'. ignores -tgz. a big file is created with everything inside.")
+	flag.Var(&xns, "xns", "regex to match and exclude unwanted namespaces. can be used multiple times.")
+	flag.Var(&xgvk, "xgvk", "regex to match and exclude unwanted groupVersion and kind. format is 'gv,k' where gv is regex to capture gv and k is regex to capture kind. ex: -xgvk metrics.*,Pod.*. can be used multiple times.")
+	flag.StringVar(&targetDir, "targetDir", filepath.FromSlash(home+"/.kube/kcdump"), "target directory where the extracted cluster data goes. directory will be recreated from scratch. a sub directory named 'cluster_info_port' is created inside the targetDir.")
+	flag.StringVar(&format, "format", "yaml", "output format. use one of: 'yaml', 'json', 'json_pretty', 'json_lines', 'json_lines_wrapped'.")
+	flag.StringVar(&kubeconfig, "kubeconfig", filepath.FromSlash(home+"/.kube/config"), "kubeconfig file or read from stdin.")
+	flag.StringVar(&context, "context", kc.CurrentContext, "kube config context to use")
+	flag.StringVar(&logLevel, "logLevel", "fatal", "use one of: 'info', 'warn', 'error', 'debug', 'panic', 'fatal'")
+
+	flag.Parse()
+
+	os.Exit(dump())
+}
+
+func dump() int {
+	log.SetLoggerLevel(logLevel)
+	kc := kcli.NewKcWithConfigContext(kubeconfig, context)
+	// fmt.Println(kc.Cluster())
+	if kc == nil {
+		fmt.Fprintf(os.Stderr, "unable to start k8s client from config file '%s' and context '%s'\n", kubeconfig, context)
+		os.Exit(-1)
+	}
+	if ns {
+		n, e := kc.Ns()
+		if e != nil {
+			return 1
+		}
+		n, e = kcli.FilterNS(n, xns, true)
+		if e != nil {
+			return 2
+		}
+		n, e = yjq.YqEval("[.items[].metadata.name] | sort | .[]", n)
+		if e != nil {
+			return 3
+		}
+		// fmt.Println("namespace")
+		fmt.Println(n)
+	}
+	if gvk {
+		g, e := kc.ApiResources()
+		if e != nil {
+			return 4
+		}
+		g, e = kcli.FilterApiResources(g, xgvk)
+		if e != nil {
+			return 5
+		}
+		g, e = yjq.YqEval(`with(.items[]; .verbs = (.verbs | to_entries)) | .items[] | select(.available and .verbs[].value == "get") | [.groupVersion + "," + .kind] | sort | .[]`, g)
+		if e != nil {
+			return 6
+		}
+		if ns {
+			fmt.Println()
+		}
+		// fmt.Println("groupVersion,kind")
+		fmt.Println(g)
+	}
+	if !ns && !gvk {
+		outputfmt, e := kcli.FormatCodeFromString(format)
+		if e != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", e.Error())
+			return 7
+		}
+		if e = kc.Dump(targetDir, xns, xgvk, nologs, gzip, tgz, prune, !dontsplitns, !dontsplitgv, outputfmt, 0, nil); e != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", e.Error())
+			return 9
+		}
+	}
+	return 0
+}
+
+func (i *nsExcludeList) String() string {
+	return fmt.Sprint(*i)
+}
+
+func (i *nsExcludeList) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
+func (i *gvkExcludeList) String() string {
+	return fmt.Sprint(*i)
+}
+
+func (i *gvkExcludeList) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
