f8789537ab135a94524547bacf2483ff6b288fc8
Mauricio Castro <macastro@redhat.com>
2024-09-03 18:56:24 -0300

    fixed --tgx viper option read

--- cmd/main.go
+++ cmd/main.go
@@ -116,7 +116,7 @@ func main() {
 	pflag.StringVar(&copyToPod, "copy-to-pod", "", "if the result of the dump is a file. a gziped json lines or a tar gziped group of directories, copy this result into the given container described as 'namespace/pod/container:/absolute_path_to_destination_file'. pod can be a substring of the target pod for which the first replica found will be used and container can be omitted for which the first container found in the pod manifest will be used. if file path ends with a '/' it will be considered a directory and source file will be copied into it. if file path is omitted all together the file will copied to '/tmp'")
 	pflag.StringVar(&filenamePrefix, "filename-prefix", "", "if the result of the dump is a file. a gziped json lines or a tar gziped group of directories, add this prefix to the file name. which will result in prefix'cluster_info_port'[.gz or .tgz]")
 	pflag.IntVar(&tailLines, "tail-log-lines", 0, "number of lines to tail the pod's logs. if -1 infinite. 0 = do not get logs (default 0)")
-	pflag.BoolVar(&showProgress, "show-progress", true, "show percentage completed in stdout")
+	pflag.BoolVar(&showProgress, "show-progress", false, "show percentage completed in stdout")
 	pflag.Parse()
 
 	log.SetLoggerLevel(logLevel)
@@ -181,11 +181,13 @@ func dump() int {
 	}
 	gvkCounter.Store(0)
 	progress := func() {
-		fmt.Printf("PROGRESS %d%%\r", (gvkCounter.Add(1)*100)/int32(totalGvk))
+		p := (gvkCounter.Add(1) * 100) / int32(totalGvk)
+		logger.Sugar().Info("PROGRESS ", p, "%")
+		if showProgress {
+			fmt.Printf("PROGRESS %d%%\r", p)
+		}
 	}
-	if !showProgress {
-		progress = nil
-	} else {
+	if showProgress {
 		fmt.Print("PROGRESS 0%\r")
 	}
 	g, e := kc.ApiResources()
@@ -233,7 +235,7 @@ func dump() int {
 func optionsFromViper() error {
 	var err error = nil
 	gzip = viper.GetBool("gzip")
-	tgz = viper.InConfig("tgz")
+	tgz = viper.GetBool("tgz")
 	prune = viper.GetBool("prune")
 	ns = viper.GetBool("ns")
 	splitns = viper.GetBool("splitns")
